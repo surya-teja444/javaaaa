284615f3414b2e60f1759e5a3c28bfb295feedc5
suryachandra <suryachandrateja444@gmail.com>
2022-12-18 17:13:28 +0530

    javvvv

---
+++ src/main/java/com/javaaproject/javaaproject/App.java
@@ -0,0 +1,13 @@
+package com.javaaproject.javaaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hell!" );
+    }
+}
